3feef43a7b358b23d1c89cff41f08478325233db
Alejandro <alejandroguadalupi@gmail.com>
2025-11-19 00:43:38 +0000

    fdbd

--- cuerpos/hugo/hugo-musculos.js
+++ cuerpos/hugo/hugo-musculos.js
@@ -64,19 +64,4 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  document.querySelectorAll('a').forEach(a => {
-    const path = a.querySelector('.musculos-hugo');
-    if (!path) return;
-    a.addEventListener('click', (e) => {
-      e.preventDefault();
-      const id = path.id;
-      resetColores();
-      if (parejas[id]) parejas[id].forEach(p => pintar(p));
-      else pintar(id);
-      setTimeout(() => {
-        const url = a.getAttribute("href") || a.getAttribute("xlink:href");
-        if (url) window.location.href = url;
-      }, 200);
-    });
-  });
 });
